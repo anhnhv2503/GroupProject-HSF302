334ca1a477e23b79bab656f4c719bcc811012ff1
anhnhv2503 <nguyenhavietanh2503@gmail.com>
2026-04-12 19:38:07 -0600

    init group project for hsf302

---
+++ src/main/java/com/project/hsf/HsfApplication.java
@@ -0,0 +1,13 @@
+package com.project.hsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsfApplication.class, args);
+	}
+
+}
